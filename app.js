71b374f9f3cc00d89c198bf7960a791cbd461f8d
25JuanP <jb061366@gmail.com>
2023-08-20 23:38:48 -0500

    proyect_01

--- app.js
+++ app.js
@@ -2,6 +2,6 @@ const functions = require('./functions.js')
 console.log(functions);
 
 console.log(functions.add(50,155));
-console.log(functions.substract(89,54));
+console.log(functions.substract(55,54));
 console.log(functions.multiply(236,845));
 console.log(functions.divide(123,897));
